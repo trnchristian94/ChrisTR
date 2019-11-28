679270e810c31435c6fe2fc4ec80e683d54d5a68
Christian <trnchristian94@gmail.com>
2019-11-28 10:56:45 +0100

    28/11/2019 | El modo nocturno se modifica a partir de ficheros scss y ya no desde variables. Creado fichero con variables para colores.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,45 +1,39 @@
-import * as React from "react";
-import "styles/app.scss";
-import "styles/defaults.scss";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavigationBar from "components/nvgtbar/NavigationBar";
 import Home from "components/nvgtbar/options/Home";
 import About from "components/nvgtbar/options/About";
 import Contact from "components/nvgtbar/options/Contact";
 
-
 import { useTranslation } from "react-i18next";
+import { useTheme } from "components/theme/ThemeContext";
 
-
-import styled from "@emotion/styled";
-
-const Wrapper = styled("div")`
-  .App-header {
-    background-color: ${(props: any) => props.theme.background};
-    color: ${(props: any) => props.theme.color};
-  }
-`;
+import "styles/app.scss";
+import "styles/defaults.scss";
+import "styles/defaultLightMode.scss";
+import "styles/defaultNightMode.scss";
 
 const App: React.FC = () => {
   const { t } = useTranslation();
-  
+  const themeState = useTheme();
 
   return (
     <Router>
-      <Wrapper>
-        <div className="App">
-          <NavigationBar />
-          <header className="App-header">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/about" component={About} />
-            </Switch>
-            <div id="reactV">R-{React.version}</div>
-            <p>{t("welcome.title")}</p>
-          </header>
-        </div>
-      </Wrapper>
+      <div
+        className={`App ${themeState.dark ? "nightMode" : "lightMode"}`}
+        id="principalApp"
+      >
+        <NavigationBar />
+        <header className="App-header">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/about" component={About} />
+          </Switch>
+          <div id="reactV">R-{React.version}</div>
+          <p>{t("welcome.title")}</p>
+        </header>
+      </div>
     </Router>
   );
 };
--- app/src/components/theme/LightSwitch.tsx
+++ app/src/components/theme/LightSwitch.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Icon from "components/core/view/Icon";
-import { useTheme } from "components/theme/ThemeContext.tsx";
+import { useTheme } from "components/theme/ThemeContext";
 import { FormCheck } from "react-bootstrap";
-import { useTranslation } from "react-i18next";
 
 export default function LightSwitch() {
-  const { t } = useTranslation();
   const themeState = useTheme();
+
+  const changeMode = () => {
+    themeState.toggle();
+    refreshMode(themeState.dark);
+  };
+
+  const refreshMode = (nightMode: boolean) => {
+    if (nightMode) {
+      window.document.getElementById("principalApp").className =
+        "App nightMode";
+    } else {
+      window.document.getElementById("principalApp").className =
+        "App lightMode";
+    }
+  };
 
   return (
     <div>
@@ -15,9 +28,9 @@ export default function LightSwitch() {
         <FormCheck.Input
           isInvalid={themeState.dark}
           checked={themeState.dark}
-          onChange={() => console.log(themeState.dark)}
+          readOnly
         />
-        <FormCheck.Label onClick={() => themeState.toggle()} />
+        <FormCheck.Label onClick={changeMode} />
         <Icon name="moon" boxSize={20} />
       </FormCheck>
     </div>
