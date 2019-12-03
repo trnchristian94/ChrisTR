75a4a77dcdc8cbbbad37bd2a18083ec02b15057f
Christian <trnchristian94@gmail.com>
2019-12-03 13:21:37 +0100

    Updates

--- src/components/nvgtbar/options/Home.tsx
+++ src/components/nvgtbar/options/Home.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
+import User from "src/Firebase/User";
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <h2>Home</h2>
+        Welcome to the Home
+        <User />
       </div>
     );
   }
