a32f62547466225d32db4985b40bbed7af8edcf5
Christian <trnchristian94@gmail.com>
2019-12-12 09:21:46 +0100

    Updates

--- src/components/nvgtbar/options/Contact.tsx
+++ src/components/nvgtbar/options/Contact.tsx
@@ -5,6 +5,7 @@ class Contact extends React.Component {
     return (
       <div>
         <h2>Contact</h2>
+        Si lo deseas puedes contactarme a los siguientes enlaces: Vale? vale
       </div>
     );
   }
